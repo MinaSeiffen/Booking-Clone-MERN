c96251fd542c455075828651bd328d838756cef4
MinaE.Seiffen <minaseiffen@gmail.com>
2024-04-11 14:18:13 +0200

    some updates for image size

--- frontend/src/Components/LatestDestinationCard.tsx
+++ frontend/src/Components/LatestDestinationCard.tsx
@@ -8,8 +8,8 @@ type Props = {
 const LatestDestinationCard = ({hotel}: Props) => {
   return (
     <Link to={`detail/${hotel._id}`} className="relative cursor-pointer overflow-hidden rounded-md">
-        <div className="h-[300px]">
-            <img src={hotel.imageUrls[0]} className="w-full h-full object-cover object-center" alt="" />
+        <div className="h-[350px]">
+            <img src={hotel.imageUrls[0]} className="w-full h-[320px] object-cover object-center" alt="" />
         </div>
         <div className="absolute bottom-0 p-4 bg-black bg-opacity-50 w-full rounded-b-md">
             <span className="text-white font-bold tracking-tighter text-3xl">
