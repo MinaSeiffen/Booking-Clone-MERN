b73593187f7fbe44f7f84c07e3a58536c408ce4c
MinaE.Seiffen <160426186+MinaSeiffen@users.noreply.github.com>
2024-06-26 10:15:20 +0300

    Update Home.tsx

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = () => {
     const topRowHotels = hotels?.slice(0,2) || [];
     const bottomRowHotels = hotels?.slice(2) || []
     return (
-        <div className="space-y-3 container w-[100vw]">
+        <div className="space-y-3 container w-[90vw]">
           <h2 className="text-3xl font-bold">Latest Destinations</h2>
           <p>Most recent desinations added by our hosts</p>
           <div className="grid gap-4">
